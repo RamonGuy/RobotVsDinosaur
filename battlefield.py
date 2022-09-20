d89d44815f5e97da0301a3ae1078a006d35802b4
RamonGuy <Ramonguy@yahoo.com>
2022-09-20 04:40:49 -0400

    Created the functions for the battlefield class

--- battlefield.py
+++ battlefield.py
@@ -1,13 +1,32 @@
+
+
 from robot import Robot
 from dinosaur import Dinosaur
-
-player1 = Robot
-player2 = Dinosaur
 class Battlefield:
     def __init__(self) -> None:
-        pass
+        self.player1 = Robot()
+        self.player2 = Dinosaur()
 
 
-def display_welcome():
-    print("Hello to the RobotVsDinosaur Game")
 
+    def display_welcome(self):
+     print("Hello to the RobotVsDinosaur Game")
+
+    def battle_phase(self):
+        self.player1.attack_dinosaur
+        self.player2.attack_robot
+        print('')
+
+
+    def display_winner(self):
+        if self.player1.health == 0:
+            print(f'{self.player2} is the winner')
+        elif self.player2 == 0:
+            print(f'{self.player1} is the winner')
+        else:
+            print("Sorry nobody won this time!!!")
+
+    def run_game(self):
+        self.display_welcome
+        self.battle_phase
+        self.display_winner
